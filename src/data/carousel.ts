a50e40d88ed9379490957bec2f997c3086ed031b
Deyverson1 <deyversongp@gmail.com>
2024-03-13 18:30:49 -0500

    Fixed problem with imports

---
+++ src/data/carousel.ts
@@ -0,0 +1,26 @@
+export const dataCarousel = [
+  {
+      title: "¿Una 4x4 a cambio de tus datos?",
+      subtitle: "Seguridad Financiera",
+      description:
+          " No pasa en la vida real. Nunca entregues tus datos, códigos de seguridad ni números de tarjetas.",
+      button: "Protégete asi",
+      image: "https://cdn.dynamicyield.com/api/8775742/images/1a7551748da9d__banner_1.jpg",
+  },
+  {
+      title: " Tus antojos no dan espera",
+      subtitle: "Tarjeta Débito Virtual ",
+      description:
+          "Pide la nueva Tarjeta Débito Virtual desde nuestra App. Préndela o apágala desde tu celu.¡Aquí y ahora, tu plata contigo!",
+      button: "Conoce más",
+      image: "https://cdn.dynamicyield.com/api/8775742/images/3809375b29dd7__banner_2_tdvirtual.jpg",
+  },
+  {
+      title: "Financia hasta el 90% de la casa que quieres",
+      subtitle: "Leasing habitacional",
+      description:
+          "Pide tu leasing habitacional no familiar con tasas más bajas que nuestro crédito de vivienda.",
+      button: " Pregunta por el tuyo",
+      image: "https://cdn.dynamicyield.com/api/8775742/images/211bff8458bda__banner_3.png",
+  },
+];
---
+++ src/data/doubts.ts
@@ -0,0 +1,31 @@
+export const dataDoubts = [
+  {
+    title: "Puntos de atención",
+    button: "Visítanos",
+    image:
+      "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/3db1f89c-3812-4955-bae5-5768bf4a5c4b/BB_multicolor_puntos_atencion+1.svg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9HC1202P86O40QTNS9EAE1S47-3db1f89c-3812-4955-bae5-5768bf4a5c4b-nwlNrq7",
+  },
+  {
+    title: "Chat en línea",
+    button: "Escríbenos",
+    image:
+      "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/fecf86a2-46a3-4d85-be63-b1392ba9f107/BB_multicolor_descripcio%CC%81n+1.svg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9HC1202P86O40QTNS9EAE1S47-fecf86a2-46a3-4d85-be63-b1392ba9f107-nwlN6Vg",
+  },
+  {
+    title: "Llámanos a nuestra línea",
+    button: "018000 91 2345",
+    image:
+      "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/d2cfd1fa-84a8-4050-bfdf-ee6f82d6d309/BB_multicolor_conoce_nuestra_app+1.svg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9HC1202P86O40QTNS9EAE1S47-d2cfd1fa-84a8-4050-bfdf-ee6f82d6d309-nwlMZEy",
+  },
+  {
+    title: "Prefieres que te llamemos",
+    button: "Cuando quieras",
+    image:
+      "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/59536b04-36a8-4850-9824-4759bb38d7f1/BB_multicolor_lineas_atencion+1.svg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9HC1202P86O40QTNS9EAE1S47-59536b04-36a8-4850-9824-4759bb38d7f1-nwlNij8",
+  },
+  {
+    title: "Contáctanos por redes sociales",
+    image:
+      "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/62a0bfec-5c38-4827-9b81-cefb288a1f59/BB_multicolor_sonido_activo+1.svg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_K9HC1202P86O40QTNS9EAE1S47-62a0bfec-5c38-4827-9b81-cefb288a1f59-nwlNHrB",
+  },
+];
