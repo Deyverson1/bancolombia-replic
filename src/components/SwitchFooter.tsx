4d3b725d4ad9e0ebc1ed1db511a94773314e5387
Deyverson1 <deyversongp@gmail.com>
2024-03-13 13:59:36 -0500

    Fixed footer on mobile

--- src/components/SwitchFooter.tsx
+++ src/components/SwitchFooter.tsx
@@ -2,7 +2,10 @@ import React, { useState } from "react"
 import { Up } from "./icons/Up";
 import { Down } from "./icons/Down";
 export default function Switch() {
-  const [isOpen, setOpen] = useState(false)
+  const [isOpen1, setOpen1] = useState(true)
+  const [isOpen2, setOpen2] = useState(true)
+  const [isOpen3, setOpen3] = useState(true)
+
   const parrafo = [
     "Acerca de nosotros",
     "Trabaja con nosotros",
@@ -35,48 +38,57 @@ export default function Switch() {
     "Línea gratuita resto del país: 01 8000 9 12345",
     "Línea ética 01 8000 524499",
   ];
-  function handleClick() {
-    setOpen(!isOpen)
-  }
+
   return (
     <>
-      <main className="paddingCard border-t-2 pt-2 md:border-t-0 md:pt-0 w-full">
-        <div className="flex justify-between text-white">
+      <main className="paddingCard border-t-2 text-white pt-2 md:border-t-0 md:pt-0 w-full">
+        <div className="flex justify-between ">
           <h3 className="pb-8">Te puede interesar</h3>
-          <div onClick={handleClick}>
-            {isOpen
+          <div onClick={() => { setOpen1(!isOpen1) }} className="md:hidden">
+            {isOpen1
               ? <Down />
               : <Up />
             }
           </div>
         </div>
         {parrafo.map((dato: string, index: number) => (
-          <li key={index} className={`${isOpen ? 'block' : 'hidden'} pb-4 md:block`}>{dato}</li>
+          <li key={index} className={`${isOpen1 ? 'block' : 'hidden'} pb-4 md:block`}>{dato}</li>
         ))
         }
       </main>
-      <div
-        className="paddingCard w-full border-t-2 pt-2 md:border-t-0 md:pt-0 flex-grow"
-      >
-        <h3 className="pb-8 w-full">Legales</h3>
-        {
-          parrafo2.map((dato: string, index: number) => (
-            <li key={index} className={`${isOpen ? 'block' : 'hidden'} pb-4 hidden md:block`}>{dato}</li>
-          ))
+      <div className="paddingCard  text-white w-full border-t-2 pt-2 md:border-t-0 md:pt-0 flex-grow">
+        <div className="flex md:block justify-between">
+          <h3 className="pb-8 w-full">Legales</h3>
+          <div onClick={() => { setOpen2(!isOpen2) }} className="md:hidden">
+            {isOpen2
+              ? <Down />
+              : <Up />
+            }
+          </div>
+        </div>
+        {parrafo2.map((dato: string, index: number) => (
+          <li key={index} className={`${isOpen2 ? 'block' : 'hidden'} pb-4 md:block`}>{dato}</li>
+        ))
         }
       </div>
-      <div className="paddingCard w-full md:pt-12">
-        {
-          parrafo3.map((dato: string, index: number) => (
-            <li key={index} className="pb-4 hidden md:block">{dato}</li>
-          ))
+      <div className="paddingCard text-white w-full md:pt-12">
+        {parrafo3.map((dato: string, index: number) => (
+          <li key={index} className={`${isOpen2 ? 'block' : 'hidden'} pb-4 md:block`}>{dato}</li>
+        ))
         }
       </div>
-      <div className="paddingCard border-t-2 pt-2 md:border-t-0 md:pt-0 w-full">
-        <h3 className="pb-4 md:pb-8">Contáctanos</h3>
-        {
-          parrafo4.map((dato, index: number) => (
-            <li key={index} className="pb-4 px-2 hidden md:block">{dato}</li>
+      <div className="paddingCard text-white border-t-2 pt-2 md:border-t-0 md:pt-0 w-full">
+        <div className="flex md:block justify-between">
+          <h3 className="pb-4 md:pb-8">Contáctanos</h3>
+          <div onClick={() => { setOpen3(!isOpen3) }} className="md:hidden">
+            {isOpen3
+              ? <Down />
+              : <Up />
+            }
+          </div>
+        </div>
+        {parrafo4.map((dato, index: number) => (
+            <li key={index} className={`${isOpen3 ? 'block' : 'hidden'} pb-4  md:block`}>{dato}</li>
           ))
         }
       </div>
