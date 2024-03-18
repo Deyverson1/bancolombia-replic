0f149306359b47f643968c3fd6621b01e20e1a3f
Deyverson1 <deyversongp@gmail.com>
2024-03-18 11:34:37 -0500

    Updates on data Carousel

--- src/data/carousel.ts
+++ src/data/carousel.ts
@@ -1,4 +1,43 @@
 export const dataCarousel = [
+    {
+        title: "4 nuevos planees para tu cuenta de ahorros",
+        subtitle: "Tú eliges",
+        description:
+            "Oro, Plata, Bronce o Acero. Abre tu cuenta o cámbiate de plan cuando lo necesites. Aquí y ahora, el poder de elegir.",
+        button: "Conocer planes",
+        image: "https://cdn.dynamicyield.com/api/8775742/images/18af88faead1f__pricing.png",
+    },
+    {
+        title: "¿Quiere carro, moto o bici?",
+        subtitle: "Encuentra tu próximo vehículo",
+        description:
+            "En Tu360Movilidad encuentras cientos de opciones de vehículos y solicita el crédito* en minutos. *Aplican TyC",
+        button: "Hazlo aquí",
+        image: "https://cdn.dynamicyield.com/api/8775742/images/9d6aa724e7c5__tu360movilidad.jpg",
+    },
+    {
+        title: "¡Bajamos la tasa al 12% E.A!",
+        subtitle: "Quédate con esa casa",
+        description:
+            "Disfrútala en proyectos financiados por nosotros con Crédito de Vivienda o Leasing Habitacional no familiar. Financiación sujeta a estudio de crédito. Aplican TyC",
+        button: "Más información",
+        image: "https://cdn.dynamicyield.com/api/8775742/images/179cf6839311c__banner_tasa_techo.png",
+    },
+    // Format
+    // {
+    //     title: "",
+    //     subtitle: "",
+    //     description:
+    //         " ",
+    //     button: "",
+    //     image: "",
+    // },
+
+];
+
+
+/*
+Old content
   {
       title: "¿Una 4x4 a cambio de tus datos?",
       subtitle: "Seguridad Financiera",
@@ -7,7 +46,7 @@ export const dataCarousel = [
       button: "Protégete asi",
       image: "https://cdn.dynamicyield.com/api/8775742/images/1a7551748da9d__banner_1.jpg",
   },
-  {
+ {
       title: " Tus antojos no dan espera",
       subtitle: "Tarjeta Débito Virtual ",
       description:
@@ -23,4 +62,4 @@ export const dataCarousel = [
       button: " Pregunta por el tuyo",
       image: "https://cdn.dynamicyield.com/api/8775742/images/211bff8458bda__banner_3.png",
   },
-];
+*/
