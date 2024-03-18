ade3bd7ffbb0cb206f25c68c1ae675892b8de657
Deyverson1 <deyversongp@gmail.com>
2024-03-18 15:40:37 -0500

    Updates

---
+++ src/components/UserHelp.tsx
@@ -0,0 +1,10 @@
+
+import { UserSearch } from "./icons/UserSearch"
+
+export default function UserHelp() {
+  return (
+    <div className="fixed z-10 bottom-3 md:bottom-6 right-6 w-fit rounded-full bg-white p-3 border-4 border-yellow-400">
+      <UserSearch />
+    </div>
+  )
+}
